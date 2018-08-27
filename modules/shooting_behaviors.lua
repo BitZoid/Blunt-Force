eef361affbed47c87bd835553537c31d2eb1069b
Unknown <bchall7299@gmail.com>
2018-08-26 21:38:19 -0500

    Misc.

--- modules/shooting_behaviors.lua
+++ modules/shooting_behaviors.lua
@@ -35,3 +35,33 @@ _ShootingPatterns[2] = function(bulletURL, obj) --shotgun
 		obj.shooting = false
 	end)
 end
+
+_ShootingPatterns[3] = function(bulletURL, obj) --roadhog
+	enemyProto.delay(.1, "waitS", function()
+		bulletProto.newBullet(bulletURL, 
+		go.get(".", "position"), 
+		enemyProto.derivativeDirection(enemyProto.target(go.get("/hero_collection/hero", "position")), -3, 3), 
+		200 + enemyProto.randomFloat(-20, 20), .5, 1,
+		function()
+			bulletProto.newBullet(bulletURL,  
+			go.get(".", "position"), 
+			enemyProto.derivativeDirection(enemyProto.randomDirection(), -100, 100),
+			200 + enemyProto.randomFloat(-20, 20), 1, 2,
+			function()
+			end)
+			bulletProto.newBullet(bulletURL,  
+			go.get(".", "position"), 
+			enemyProto.derivativeDirection(enemyProto.randomDirection(), -100, 100),
+			200 + enemyProto.randomFloat(-20, 20), 1, 2,
+			function()
+			end)
+			bulletProto.newBullet(bulletURL,  
+			go.get(".", "position"), 
+			enemyProto.derivativeDirection(enemyProto.randomDirection(), -100, 100),
+			200 + enemyProto.randomFloat(-20, 20), 1, 2,
+			function()
+			end)
+		end)
+		obj.shooting = false
+	end)
+end
